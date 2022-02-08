ce08e9bb46496d1c1fdce3770fed329fa461f4d0
Hyun Jae Cho <50690938+chohyunjae163@users.noreply.github.com>
2022-02-08 12:40:41 +0000

    added lazyseq file

--- main.clj
+++ main.clj
@@ -26,5 +26,14 @@
         (set [(+ fst 4)])))))))
 
 
-(println "done")
+(def human-consumption [8.1 7.3 6.6 5.0])
+(def critter-consumption [0.0 0.2 0.3 1.1])
+(defn unify-diet-data
+  [human critter]
+  {:human human
+   :critter critter})
 
+(println(map unify-diet-data human-consumption critter-consumption))
+
+
+(println "done")
