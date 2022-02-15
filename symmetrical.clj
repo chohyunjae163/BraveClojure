1a7602e11a8daca15c1cb49ddd7b675cb5819c2c
Hyun Jae Cho <50690938+chohyunjae163@users.noreply.github.com>
2022-02-15 15:24:02 +0000

    added defs for comparing conditions. maybe too verbose?

--- symmetrical.clj
+++ symmetrical.clj
@@ -5,27 +5,26 @@
   [s]  
   (apply str (reverse s)))
 
-(defn add2 
-  [n] 
-  (inc (inc n)))
-
+(def add2 #(inc (inc %)))
 (def mod2 #(mod % 2))
 (def mod8 #(mod % 8))
 (def quot2 #(quot % 2))
 (def quot8 #(quot % 8))
+(def lessThanTwo? #(< % 2))
+(def lessThanEight? #(< % 8))
 
 (defn decimalToBinary
   [n]
-  (if (< n 2)
+  (if (lessThanTwo? n)
     (str n)
     (str (decimalToBinary (quot2 n)) (mod2 n))))
 
 (defn decimalToOctal
   [n]
-  (if (< n 8)
+  (if (lessThanEight? n)
     (str n)
     (str (decimalToOctal (quot8 n)) (mod8 n))))
-
+ 
 (defn palindromicDecimal? 
   [n]
   (= (str n) (str-reverse (str n))))
@@ -41,7 +40,8 @@
    (= octal (str-reverse octal))))
 
 
-(def smallestPalindromicNumber 
+(def smallestPalindromicNumber
+  "number starts at 11 and is added by 2. only the odd number could be palindrome for binary numbers"
   (loop [number 11]
     (if (and (palindromicDecimal? number)
              (palindromicBinary? number)
@@ -50,6 +50,7 @@
       (recur (add2 number)))))
 
 
-(println smallestPalindromicNumber)
+;;(println smallestPalindromicNumber)
 
 ;;the answer should be 585
+;;decimal 585 - binary 1001001001 - octal :1111
