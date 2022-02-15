04da12c2c69e5fe2acf637556d5ed91fe0e597c0
Hyun Jae Cho <50690938+chohyunjae163@users.noreply.github.com>
2022-02-15 14:42:35 +0000

    finally solved the problem... find the smallest palindromic number in binary , octal, and decimal

--- symmetrical.clj
+++ symmetrical.clj
@@ -25,6 +25,19 @@
   (inc (inc n)))
 
 (def mod2 #(mod % 2))
+(def mod8 #(mod % 8))
+
+(defn decimalToBinary
+  [n]
+  (if (< n 2)
+    (str n)
+    (str (decimalToBinary (quot n 2)) (mod2 n))))
+
+(defn decimalToOctal
+  [n]
+  (if (< n 8)
+    (str n)
+    (str (decimalToOctal (quot n 8)) (mod8 n))))
 
 (defn palindromicDecimal? 
   [n]
@@ -32,27 +45,22 @@
 
 (defn palindromicBinary? 
   [n]
-  (= (str n) (str-reverse (str n))))
+  (let [binary (decimalToBinary n)]
+    (= binary (str-reverse binary))))
 
 (defn palindromicOctal? 
   [n]
-  (= (str n) (str-reverse (str n))))
-
-(defn decimalToBinary
-  [n]
-  (if (= n 0)
-    0
-    (mod n 2)))
+  (let [octal (decimalToOctal n)]
+   (= octal (str-reverse octal))))
 
 
-
-(println(mod2 7))
-
-(defn smallestPalindromicNumber 
-  []
-  (loop [number 1]
+(def smallestPalindromicNumber 
+  (loop [number 11]
     (if (and (palindromicDecimal? number)
              (palindromicBinary? number)
              (palindromicOctal? number))
       number
       (recur (add2 number)))))
+
+
+(println smallestPalindromicNumber)
